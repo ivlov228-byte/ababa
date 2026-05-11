16701211ee1be7688f181a7f5890477c44ddcd60
ivlov228_byte <ivlov228@gmail.com>
2026-05-11 20:41:41 +0400

    Исправленная версия подсчета Фибоначчи, чтобы подсчет начинался со значений (0,1,..)

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-n = int(input("n ="))
+n = int(input("n = "))
 def fib(n):
-    if n <= 0:
+    if n == 1:
         return 0
-    elif n == 1 or n == 2:
+    elif n == 2:
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
